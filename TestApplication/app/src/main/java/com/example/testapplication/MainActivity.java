4e916a4945249d48a01ef5222b0f41c1fdc00e0b
PATHUM <mspathum@gmail.com>
2023-06-12 17:38:02 +0900

    Testing screenshot No 1 Task

---
+++ TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -0,0 +1,206 @@
+package com.example.testapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.core.VideoCapture;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+
+public class MainActivity extends AppCompatActivity implements ImageAnalysis.Analyzer, View.OnClickListener  {
+
+    private static int CAMERA_PERMISSION_CODE = 100;
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    PreviewView previewView;
+    private ImageCapture imageCapture;
+    private VideoCapture videoCapture;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout chartLinearLayout = findViewById(R.id.sslayout);
+//      LinearLayout chartLinearLayout = findViewById(R.id.chart);
+        Button screenShotButton = findViewById(R.id.Btn1);
+        previewView = findViewById(R.id.previewView);
+
+        if (isCameraPresentInPhone()) {
+            Log.i("VIEDEO_RECORD_TAG", "Camera is detected");
+            getCameraPermission();
+        } else {
+            Log.i("VIEDEO_RECORD_TAG", "No Camera is detected");
+        }
+
+        screenShotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap screenshot = captureScreenShot(chartLinearLayout);
+                storeImageAsJPEG(screenshot);
+            }
+        });
+
+        //CameraX
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, getExecutor());
+
+    }
+
+    //CameraX
+    Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    //CameraX
+    @SuppressLint("RestrictedApi")
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        Preview preview = new Preview.Builder()
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        // Image capture use case
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+
+        // Video capture use case
+        videoCapture = new VideoCapture.Builder()
+                .setVideoFrameRate(30)
+                .build();
+
+        // Image analysis use case
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(getExecutor(), this);
+
+        //bind to lifecycle:
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture, videoCapture);
+    }
+
+    //CameraX
+    @Override
+    public void analyze(@NonNull ImageProxy image) {
+        // image processing here for the current frame
+        Log.d("TAG", "analyze: got the frame at: " + image.getImageInfo().getTimestamp());
+        image.close();
+    }
+
+    //CameraX
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onClick(View view) {
+    }
+
+
+    //ScreenShot Image Capture
+    public Bitmap captureScreenShot(View view){
+        Bitmap returnBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnBitmap);
+        Drawable bgDrawable = view.getBackground();
+
+        if(bgDrawable!=null){
+            bgDrawable.draw(canvas);
+        } else {
+            canvas.drawColor(Color.WHITE);
+            view.draw(canvas);
+        }
+        return  returnBitmap;
+    }
+
+    //ScreenShot Image Capture
+    public void storeImageAsJPEG(Bitmap bitmap) {
+        OutputStream outst;
+        try {
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.Q) {
+                ContentResolver contentResolver = getContentResolver();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Image_"+".jpg");
+                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+                contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + File.separator + "TestFolder" );
+                Uri imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+                outst = contentResolver.openOutputStream(Objects.requireNonNull(imageUri));
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outst);
+                Objects.requireNonNull(outst);
+
+                Log.i("VIEDEO_RECORD_TAG", "Screenshot Successful");
+                Toast.makeText(getApplicationContext(), "Screen Shot Saved successful", Toast.LENGTH_LONG).show();
+            } else {
+                Log.i("VIEDEO_RECORD_TAG", "Screenshot Unsuccessful");
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //ScreenShot Image Capture
+    private boolean isCameraPresentInPhone() {
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //ScreenShot Image Capture
+    private void getCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+    }
+
+}
